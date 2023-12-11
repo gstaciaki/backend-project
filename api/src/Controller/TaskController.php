40b005915399cda98e3baaaf29c9169b76ce2023
Guilherme Staciaki <gstaciaki99@gmail.com>
2023-12-10 23:16:23 -0300

    criado get e post tasks

--- api/src/Controller/TaskController.php
+++ api/src/Controller/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Models\Task;
+use DateTime;
 
 class TaskController
 {
@@ -108,11 +109,18 @@ class TaskController
                 t.id, t.title, t.created_at, t.finished_at, t.due_date, t.priority
         ');
 
-            $taskData = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            $stmt->bindParam(':taskId', $taskId);
+            $stmt->execute();
+
+            $taskData = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if (!$taskData) {
+                return ['error' => 'Task not found'];
+            }
 
             $owners = json_decode($taskData['owners'], true);
 
-            // Construir a Task com o array de owners
+            // Construct the Task with the array of owners
             $task = new Task(
                 $taskData['task_id'],
                 $taskData['title'],
@@ -123,7 +131,7 @@ class TaskController
                 $owners
             );
 
-            // Converta a tarefa para um array associativo
+            // Convert the task to an associative array
             $taskArray = [
                 'id' => $task->getId(),
                 'title' => $task->getTitle(),
@@ -134,32 +142,72 @@ class TaskController
                 'owners' => $task->getOwners(),
             ];
 
-            return [$taskArray];
+            return $taskArray;
         } catch (\PDOException $e) {
-            return ['error' => 'Erro ao obter as tarefas: ' . $e->getMessage()];
+            return ['error' => 'Error fetching task: ' . $e->getMessage()];
         }
     }
 
-
-
-
-    public function createNewTask(Task $task)
+    public function createNewTask($title)
     {
         try {
+            $createdAt = new DateTime();
+            $formattedCreatedAt = $createdAt->format('Y-m-d H:i:s');
 
             $stmt = $this->pdo->prepare('
-                INSERT INTO tasks (title, created_at, finished_at, due_date, priority) 
-                VALUES (?, ?, ?, ?, ?)
-            ');
+            INSERT INTO tasks (title, created_at) 
+            VALUES (?, ?)
+        ');
             $stmt->execute([
-                $task->getTitle(),
-                $task->getCreatedAt(),
-                $task->getFinishedAt(),
-                $task->getDueDate(),
-                $task->getPriority()
+                $title,
+                $formattedCreatedAt,
             ]);
         } catch (\PDOException $e) {
-
+            // Lidar com erros, por exemplo, lançando uma exceção ou registrando o erro
         }
     }
+
+    // Adicione este método à classe TaskController
+public function updateTaskObject(Task $task, array $owners)
+{
+    try {
+        $this->pdo->beginTransaction();
+
+        // Atualize os detalhes da tarefa (title, due_date, priority)
+        $stmtTask = $this->pdo->prepare('
+            UPDATE tasks 
+            SET title = :title, due_date = :dueDate, priority = :priority 
+            WHERE id = :taskId
+        ');
+
+        $stmtTask->bindParam(':title', $task->getTitle());
+        $stmtTask->bindParam(':dueDate', $task->getDueDate());
+        $stmtTask->bindParam(':priority', $task->getPriority());
+        $stmtTask->bindParam(':taskId', $task->getId());
+
+        $stmtTask->execute();
+
+        // Exclua os proprietários antigos
+        $stmtDeleteOwners = $this->pdo->prepare('DELETE FROM task_users WHERE task_id = :taskId');
+        $stmtDeleteOwners->bindParam(':taskId', $task->getId());
+        $stmtDeleteOwners->execute();
+
+        // Insira os novos proprietários
+        $stmtInsertOwners = $this->pdo->prepare('INSERT INTO task_users (task_id, user_id) VALUES (:taskId, :userId)');
+        $stmtInsertOwners->bindParam(':taskId', $task->getId());
+
+        foreach ($owners as $owner) {
+            $stmtInsertOwners->bindParam(':userId', $owner['user_id']);
+            $stmtInsertOwners->execute();
+        }
+
+        $this->pdo->commit();
+
+        return ['message' => 'Task updated successfully'];
+    } catch (\PDOException $e) {
+        $this->pdo->rollBack();
+        return ['error' => 'Error updating task and owners: ' . $e->getMessage()];
+    }
+}
+
 }
--- api/src/router.php
+++ api/src/router.php
@@ -1,81 +1,119 @@
 <?php
 
 use App\Controller\TaskController;
+use App\Models\Task;
 
 require_once 'Controller/TaskController.php';
 
 
 function handleRequest($method, $uri)
 {
-    if ($method === 'GET' && $uri === '/api/rota') {
-        return handleGetRoute();
-    } elseif ($method === 'POST' && $uri === '/api/rota') {
-        return handlePostRoute();
-    } elseif ($method === 'GET' && $uri === '/api/test-database') {
-        return handleTestDatabaseConnection();
-    } elseif ($method === 'GET' && $uri === '/api/tasks') {
+    if ($method === 'GET' && $uri === '/api/tasks') {
         return handleGetTasks();
-    } elseif ($method === 'GET' && strpos($uri, '/api/tasks/') === 0) {
-        $taskId = intval(substr($uri, 12));
-
-        return handleGetTasks($taskId);
-    } else {
-        http_response_code(404);
-        return ['erro' => 'Rota não encontrada'];
     }
+    if ($method === 'POST' && $uri === '/api/tasks') {
+        return handlePostTask();
+    }
+    if ($method === 'GET' && strpos($uri, '/api/tasks/') === 0) {
+        $taskId = intval(substr($uri, 11));
+        return handleGetTasks($taskId);
+    }
+    if ($method === 'PUT' && strpos($uri, '/api/tasks/') === 0) {
+        $taskId = intval(substr($uri, 11));
+        return handleUpdateTask($taskId);
+    }
+
+    http_response_code(404);
+    return ['erro' => 'Rota não encontrada'];
+
 }
 
-
-function handleGetRoute()
-{
-    return ['mensagem' => 'Rota GET acessada com sucesso'];
-}
-
-function handlePostRoute()
+function handlePostTask()
 {
     $dados = json_decode(file_get_contents("php://input"), true);
-    return ['mensagem' => 'Rota POST acessada com sucesso', 'dados' => $dados];
-}
-
-function handleTestDatabaseConnection()
-{
     require_once 'config.php';
 
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $connected = $pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+        $taskController = new TaskController($pdo);
 
-        if ($connected) {
-            return ['mensagem' => 'Conexão com o banco de dados estabelecida com sucesso'];
-        } else {
-            return ['erro' => 'Falha na conexão com o banco de dados'];
-        }
+        $tasks = $taskController->createNewTask($dados['title']);
+
+        // Retornar as tarefas
+        return ['mensagem' => 'Task inserida com sucesso', 'dados' => $dados];
     } catch (PDOException $e) {
+        // Lidar com erros de conexão com o banco de dados
         return ['erro' => 'Falha na conexão com o banco de dados: ' . $e->getMessage()];
     }
 }
 
-function handleGetTasks($taskId = null)
+function handleUpdateTask($taskId)
 {
-    // Configuração do banco de dados
+    $dados = json_decode(file_get_contents("php://input"), true);
     require_once 'config.php';
 
     try {
-        // Conectar ao banco de dados
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // Instanciar o TaskController com o objeto PDO
+        $taskController = new TaskController($pdo);
+
+        // Obter a tarefa existente
+        $existingTaskData = $taskController->getTaskById($taskId);
+
+        if (isset($existingTaskData['error'])) {
+            http_response_code(404);
+            return ['error' => 'Tarefa não encontrada'];
+        }
+
+        // Criar uma instância de Task com os dados existentes
+        $existingTask = new Task(
+            $existingTaskData['id'],
+            $existingTaskData['title'],
+            $existingTaskData['created_at'],
+            $existingTaskData['finished_at'],
+            $existingTaskData['due_date'],
+            $existingTaskData['priority'],
+            $existingTaskData['owners']
+        );
+
+        // Atualizar apenas as propriedades não nulas do objeto Task
+        foreach ($dados as $key => $value) {
+            if (!is_null($value)) {
+                $existingTask->{$key} = $value;
+            }
+        }
+
+        // Atualizar a tarefa no banco de dados
+        $result = $taskController->updateTaskObject($existingTask);
+
+        if (isset($result['error'])) {
+            http_response_code(500);
+        }
+
+        return $result;
+    } catch (PDOException $e) {
+        // Lidar com erros de conexão com o banco de dados
+        return ['erro' => 'Falha na conexão com o banco de dados: ' . $e->getMessage()];
+    }
+}
+
+
+function handleGetTasks($taskId = null)
+{
+    require_once 'config.php';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         $taskController = new TaskController($pdo);
 
         if ($taskId !== null) {
-            // Obter uma tarefa específica pelo ID
             $task = $taskController->getTaskById($taskId);
 
-            // Retornar a tarefa específica
             return $task;
         }
 
